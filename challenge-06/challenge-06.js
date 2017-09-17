d5300e305b26812c818b025f4a2ba195bd467e1e
Arthur Souza <arthur_vitor16@outlook.com>
2017-09-17 00:26:47 -0300

    Resolution challenge 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,12 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+// ES5
+var championship = 'Copa Mundial';
+
+// ES6
+// let championship = 'Copa Mundial';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,8 +17,11 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+// ES5
+var teams = ['Brasil', 'Alemanha', 'China', 'EUA', 'Agentina']
 
+// ES6
+//let teams = ['Brasil', 'Alemanha', 'China', 'EUA', 'Agentina']
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
@@ -32,19 +40,46 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+// ES5
+function showTeamPosition(position) {
+    var realPosition = position - 1;
+    if(realPosition < 0 || realPosition > teams.length)
+        return "Não temos a informação do time que está nessa pósição."
+    
+    return "O time que está em " + position + "º lugar é o " + teams[realPosition] + "."
+}
+
+// ES6
+// function showTeamPosition(position) {
+//     let realPosition = position--;
+//     if(realPosition < 0 && realPosition > teams.length)
+//         return "Não temos a informação do time que está nessa pósição."
+    
+//     return `O time que está em ${position}º lugar é o ${teams[realPosition]}.`
+// }
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+// ES5 and ES6
+console.log(showTeamPosition(1));
+console.log(showTeamPosition(3));
+console.log(showTeamPosition(2));
+console.log(showTeamPosition(8));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+// ES5 and ES6
+var counter = 20; // ES5
+//let counter = 20; // ES6
+
+while (counter <= 30) {
+    console.log(counter);
+    counter++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +93,51 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+// ES5
+function convertToHex(cor) {
+    switch (cor) {
+        case 'preto':
+            return "O hexadecimal para a cor " + cor + " é #000000";
+        case 'branco': 
+            return "O hexadecimal para a cor " + cor + " é #FFFFFF";
+        case 'vermelho':
+            return "O hexadecimal para a cor " + cor + " é #FF0000";
+        case 'azul':
+            return "O hexadecimal para a cor " + cor + " é #0000FF";
+        case 'rosa':
+            return "O hexadecimal para a cor " + cor + " é #FF1493";
+        default:
+            return "Não temos o equivalente hexadecimal para " + cor + "."
+    }
+}
+
+// // ES6
+// function convertToHex(cor) {
+//     switch (cor) {
+//         case 'preto':
+//             return `O hexadecimal para a cor ${cor} é #000000`;
+//         case 'branco': 
+//             return `O hexadecimal para a cor ${cor} é #FFFFFF`;
+//         case 'vermelho':
+//             return `O hexadecimal para a cor ${cor} é #FF0000`;
+//         case 'azul':
+//             return `O hexadecimal para a cor ${cor} é #0000FF`;
+//         case 'rosa':
+//             return `O hexadecimal para a cor ${cor} é #FF1493`;
+//         default:
+//             return `Não temos o equivalente hexadecimal para ${cor}.`
+//     }
+// }
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+// ES6 and ES5
+console.log(convertToHex('preto'));
+console.log(convertToHex('verde'));
+console.log(convertToHex('azul'));
+console.log(convertToHex('amarelo'));
+console.log(convertToHex('roxo'));
+console.log(convertToHex('rosa'));
+console.log(convertToHex('vermelho'));
+console.log(convertToHex('branco'));
